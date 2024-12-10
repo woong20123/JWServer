f82581a41885a0244e992d9eae83a1a17e5e6e2c
woong20123 <jinny20123@gmail.com>
2024-12-10 23:32:26 +0900

    SampleClient 프로젝트 추가

---
+++ JWServer/SampleClient/LoginWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SampleClient
+{
+    /// <summary>
+    /// StartWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        TcpClient? client = null;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        async private void SessionConnect(string ip, int port)
+        {
+            client = new TcpClient();
+            await client.ConnectAsync(ip, port);
+            MessageBox.Show("서버 연결에 성공하였습니다.", "Information");
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string addressInfoText = addressInputText.Text;
+                string ip = "";
+                int port = 0;
+                if (addressInfoText.Contains(':'))
+                {
+                    var addressInfos = addressInfoText.Split(':');
+                    ip = addressInfos[0];
+                    port = int.Parse(addressInfos[1]);
+                }
+                SessionConnect(ip, port);
+
+            }
+            catch
+            {
+                MessageBox.Show("서버 연결에 실패하였습니다.", "Server Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                client = null;
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+            this.Close();
+        }
+    }
+}
--- makeProtoBuffer.bat
+++ makeProtoBuffer.bat
@@ -1,3 +1,4 @@
-external\protobuf\protoc\protoc.exe --cpp_out=JWServer\Packet\GamePacket --proto_path=JWServer\Packet\ GamaPacket.proto
+external\protobuf\protoc\protoc.exe --cpp_out=JWServer\Packet\GamePacket\Cpp --proto_path=JWServer\Packet\ GamaPacket.proto
+external\protobuf\protoc\protoc.exe --csharp_out=JWServer\Packet\GamePacket\CSharp --proto_path=JWServer\Packet\ GamaPacket.proto
 
 pause
